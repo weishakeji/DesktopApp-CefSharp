abf8b488196214962bbb978d051b0a0ce9fa3942
weishakeji <10522779@qq.com>
2019-02-19 16:54:01 +0800

    引用webkit.net

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new WebKit.WebKitBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.BackColor = System.Drawing.Color.White;
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(868, 557);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 534);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private WebKit.WebKitBrowser webBrowser1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,5 +15,19 @@ namespace DesktopApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "测试窗体";
+            //
+            string appPath = System.Environment.CurrentDirectory;
+            string url = appPath + "\\..\\..\\WebPage\\test.html";
+            if (System.IO.File.Exists(url))
+            {
+                this.webBrowser1.Navigate(url);
+            }
+            this.webBrowser1.Url = new System.Uri(url, System.UriKind.Absolute);
+            //this.webBrowser1.Navigate("http://www.168fff.cn");
+        }
     }
 }
