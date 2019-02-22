abf16c8043c266819d78b1c493d85608aa73cd5f
weishakeji <10522779@qq.com>
2019-02-22 17:16:32 +0800

    添加获取CPU串号的方法

---
+++ DesktopApp/Handler/Client.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using Microsoft.Win32;
+using System.Reflection;
+
+namespace DesktopApp.Handler
+{
+    public class Client
+    {
+        public static string CPUCode
+        {
+            get
+            {
+                string strCpu = null;
+                //取CPU物理串号
+                try
+                {
+
+                    ManagementClass myCpu = new ManagementClass("win32_Processor");  //获取系统CPU处理器
+                    ManagementObjectCollection myCpuConnection = myCpu.GetInstances();
+                    foreach (ManagementObject myObject in myCpuConnection)
+                    {
+                        if (myObject.Properties["Processorid"] != null)
+                        {
+                            strCpu = myObject.Properties["Processorid"].Value.ToString();   //获取CPU序列号
+                            break;
+                        }
+                    }
+                }
+                catch { }
+                //如果实在取不到
+                if (string.IsNullOrWhiteSpace(strCpu))
+                {
+                    strCpu = "notGetCPU";
+                }
+                return strCpu;
+            }
+        }
+    }
+}
