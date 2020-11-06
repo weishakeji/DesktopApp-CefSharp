26b2a69bfae310365cc27a36e3f4f26d915d6d4c
weishakeji <songleiming@163.com>
2020-11-06 09:23:32 +0800

    增补注释

--- DesktopApp/LoginForm.cs
+++ DesktopApp/LoginForm.cs
@@ -10,7 +10,17 @@ using System.Threading;
 using System.Windows.Forms;
 using CefSharp;
 using CefSharp.WinForms;
+/*
+    产品推荐：《微厦在线学习系统》
 
+    “在线视频学习、在线试题练习、在线同步考试”紧密相联，    
+    形成“学、练、考”于一体的在线教育系统。
+
+    可私有化部署，对接支付宝支付、微信支付，收益全掌控；
+    多终端支持，电脑、手机、微信、APP、微信小程序，全都有；
+
+    永久授权，一次购买，终身使用；
+*/
 namespace DesktopApp
 {
     public partial class LoginForm : Form
--- DesktopApp/LoginFunction/Other_login.cs
+++ DesktopApp/LoginFunction/Other_login.cs
@@ -3,7 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+    产品推荐：《微厦在线学习系统》
 
+    “在线视频学习、在线试题练习、在线同步考试”紧密相联，    
+    形成“学、练、考”于一体的在线教育系统。
+
+    可私有化部署，对接支付宝支付、微信支付，收益全掌控；
+    多终端支持，电脑、手机、微信、APP、微信小程序，全都有；
+
+    永久授权，一次购买，终身使用；
+*/
 namespace DesktopApp.LoginFunction
 {
     /// <summary>
